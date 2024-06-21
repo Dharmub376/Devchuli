1dfeca904f9c08348f8607a0671b3511640931c8
dharmu <dharmub376@gmail.com>
2024-06-21 15:46:19 +0545

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
